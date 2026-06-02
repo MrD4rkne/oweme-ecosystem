01b5cba510a74066362f7b8ecc0aa95e67a2def4
Marcin Szopa <marcinsz.pv@gmail.com>
2026-06-02 13:43:19 +0200

    chore: add identity server and web API projects to application builder

--- src/OweMe.Ecosystem/OweMe.Ecosystem.AppHost/AppHost.cs
+++ src/OweMe.Ecosystem/OweMe.Ecosystem.AppHost/AppHost.cs
@@ -1,3 +1,9 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var identityServer = builder.AddProject<Projects.OweMe_Identity_Server>("identityserver");
+
+builder.AddProject<Projects.OweMe_Api>("webapi")
+       .WithReference(identityServer)
+       .WithEnvironment("IdentitySettings__Authority", identityServer.GetEndpoint("https")); 
+
 builder.Build().Run();
