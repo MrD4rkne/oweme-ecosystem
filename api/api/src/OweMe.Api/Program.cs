43c7b59782989e36f996bb2d79a515f0e9702c48
Marcin Szopa <marcinsz.pv@gmail.com>
2026-09-11 19:12:46 +0200

    fix: reorder middleware for proper authorization handling

--- api/api/src/OweMe.Api/Program.cs
+++ api/api/src/OweMe.Api/Program.cs
@@ -135,8 +135,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
-app.UseAuthorization();
 app.UseMiddleware<UserContextMiddleware>();
+app.UseAuthorization();
 
 app.MapEndpoints();
 
